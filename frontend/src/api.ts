2e9ca1fad8a181662e0b133123d337f06a642a47
Anass El Achham <anas.elachham@gmail.com>
2025-08-27 16:30:13 +0100

    V2.0

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,13 +1,12 @@
 import axios from 'axios';
 import { Candidate } from './types';
 
-// The base URL of our Django API.
-const API_BASE_URL = 'http://localhost:8000/api';
+// Dans Docker, utiliser le nom du service backend, sinon localhost
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
 
 /**
  * @param skill - Optional skill to filter by.
  */
-
 export const getCandidates = async (skill: string = ''): Promise<Candidate[]> => {
     try {
         let url = `${API_BASE_URL}/candidates/`;
@@ -27,7 +26,6 @@ export const getCandidates = async (skill: string = ''): Promise<Candidate[]> =>
  * Creates a new candidate by sending form data to the backend.
  * @param formData - The form data containing candidate information and the CV file.
  */
-
 export const createCandidate = async (formData: FormData): Promise<Candidate> => {
     const response = await axios.post<Candidate>(`${API_BASE_URL}/candidates/`, formData, {
         headers: {
